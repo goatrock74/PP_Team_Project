05e8c644ba5e68f6907e19cd50032c56a73828d6
jangbari1117 <ssjanghy@gmail.com>
2026-09-03 20:26:03 +0900

    weatherPassive

--- Assets/JHY/Scripts/SeasonPeriod.cs
+++ Assets/JHY/Scripts/SeasonPeriod.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -9,7 +10,8 @@ public class SeasonPeriod : MonoBehaviour
     [SerializeField] private GameObject winter;
 
     [SerializeField] private GameObject DarkPannel;
-    public void ChangeSeasonPeriod(TimeManager.SeasonPeriod newSeason)
+
+    public void ChangeSeasonPeriod(TimeManager.SeasonPeriod newSeason)//타일맵 swap + 각 계절별 제철 과일들로 상점 갱신
     {
         switch (newSeason)
         {
@@ -20,6 +22,7 @@ public class SeasonPeriod : MonoBehaviour
                 summer.SetActive(false);
                 autumn.SetActive(false);
                 winter.SetActive(false);
+                //상점 갱신
                 break;
             case TimeManager.SeasonPeriod.Summer:
                 StartCoroutine(FadeInOut());
@@ -28,14 +31,16 @@ public class SeasonPeriod : MonoBehaviour
                 summer.SetActive(true);
                 autumn.SetActive(false);
                 winter.SetActive(false);
+                //상점갱신
                 break;
-            case TimeManager.SeasonPeriod.Authum:
+            case TimeManager.SeasonPeriod.Autumn:
                 StartCoroutine(FadeInOut());
-                Debug.Log("Authum");
+                Debug.Log("Autumn");
                 spring.SetActive(false);
                 summer.SetActive(false);
                 autumn.SetActive(true);
                 winter.SetActive(false);
+                //상점갱신
                 break;
             case TimeManager.SeasonPeriod.Winter:
                 StartCoroutine(FadeInOut());
@@ -44,6 +49,7 @@ public class SeasonPeriod : MonoBehaviour
                 summer.SetActive(false);
                 autumn.SetActive(false);
                 winter.SetActive(true);
+                //상점갱신
                 break;
         }
     }
--- Assets/JHY/Scripts/TimePeriod.cs
+++ Assets/JHY/Scripts/TimePeriod.cs
@@ -6,8 +6,8 @@ public class TimePeriod : MonoBehaviour
     [SerializeField] private CanvasGroup afternoon;
     [SerializeField] private CanvasGroup night;
    
-    public void ChangeTimePeriod(TimeManager.TimePeriod newPeriod) //페이드인 페이드아웃 서서히 사라지는걸로
-    {
+    public void ChangeTimePeriod(TimeManager.TimePeriod newPeriod) //global light 2d로 밝기 조절 + 시간대별 상점 갱신
+    { //시간대 별로도 상점 갱신해야되고 계절 바뀔떄도 상점 갱신해야되니까 상점 매니저를 따로 만들어서 쓰기.
         switch (newPeriod)
         {
             case TimeManager.TimePeriod.Morning:
@@ -15,18 +15,21 @@ public class TimePeriod : MonoBehaviour
                 sun.alpha = 1;
                 afternoon.alpha = 0;
                 night.alpha = 0;
+                //상점 갱신
                 break;
             case TimeManager.TimePeriod.Afternoon:
                 Debug.Log("afternoon");
                 sun.alpha = 0;
                 afternoon.alpha = 1;
                 night.alpha = 0;
+                //상점 갱신
                 break;
             case TimeManager.TimePeriod.Night:
                 Debug.Log("night");
                 sun.alpha = 0;
                 afternoon.alpha = 0;
                 night.alpha = 1;
+                //상점 갱신
                 break;
         }
     }
