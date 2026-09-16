f36f1b91749ce9e479cb7ff18ed8bb98f1a15de8
goatrock74 <jeongub112@gmail.com>
2026-09-16 23:55:48 +0900

    commit32

--- Assets/PJW/Script/SO-Script/Item.cs
+++ Assets/PJW/Script/SO-Script/Item.cs
@@ -11,10 +11,9 @@ public class Item : ScriptableObject
 
     [field: SerializeField] public int Item_price { get; private set; }
 
-    [Header("�Ǹ� ���� (0���� �θ� ���Ű��� �������� �ڵ� ���)")]
+    [Header("price")]
     [SerializeField] private int item_sellPrice = 0;
 
-    // ���� �Ǹ� �� ����ϴ� ����. �ν����Ϳ� ���� �� ������ ���Ű��� �������� �ڵ� ����
     public int Item_SellPrice => item_sellPrice > 0 ? item_sellPrice : Item_price / 2;
 
     [field: SerializeField] public string Item_name { get; private set; }
