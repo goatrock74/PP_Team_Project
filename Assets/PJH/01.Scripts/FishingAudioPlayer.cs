77aa04fa84826c8d29dae99157aa45cf4f073b55
goatrock74 <jeongub112@gmail.com>
2026-09-26 18:29:29 +0900

    commit57

--- Assets/PJH/01.Scripts/FishingAudioPlayer.cs
+++ Assets/PJH/01.Scripts/FishingAudioPlayer.cs
@@ -52,9 +52,6 @@ namespace PJH._01.Scripts
         {
             reelLoopSource.Stop();
         }
-<<<<<<< HEAD
-=======
-        
         public void PlayRetrieve()
         {
             if (sfxSource == null)
@@ -86,6 +83,5 @@ namespace PJH._01.Scripts
         }
         
         
->>>>>>> Base
     }
 }
